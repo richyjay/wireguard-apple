c6ec661ec833702a549a3d6e173f6d4c469c2ded
richyjay <richard.ablewhite@gmail.com>
2021-04-19 15:53:23 +0100

    Add System Extension

---
+++ WireGuardSystemExtension/main.swift
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2018-2020 WireGuard LLC. All Rights Reserved.
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
